0e456daef50ea658f27e7d734a1cb2a4fcba9e76
Rodrigo Albano Hofer Bastos <digoalbano1010@gmail.com>
2024-07-23 13:13:55 -0300

    feat: mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      'myFont': ['myFont'],
+      'myFontBold': ['myFontBold'],
+      'myFontIta': ['myFontIta'],
+      'myFontMed': ['myFontMed']
+    },
+    extend: {
+      colors:{
+        "lime": "var(--lime)",
+        "primary-red": "var(--red)",
+        "primary-white": "var(--white)",
+        "slate-100": "var(--slate-100)",
+        "slate-300": "var(--slate-300)",
+        "slate-500": "var(--slate-500)",
+        "slate-700": "var(--slate-700)",
+        "slate-900": "var(--slate-900)"
+      }
+    },
   },
   plugins: [],
 }
